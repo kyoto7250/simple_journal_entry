acb169fa8c70017101d2d708516f07c7ba83f8fa
nariura.takuto <50972773+kyoto7250@users.noreply.github.com>
2022-04-22 13:53:24 +0900

    fix test

--- api/src/test/kotlin/com/okeicalm/simplejournalentry/usecase/account/AccountCreateUseCaseTests.kt
+++ api/src/test/kotlin/com/okeicalm/simplejournalentry/usecase/account/AccountCreateUseCaseTests.kt
@@ -17,8 +17,8 @@ class AccountCreateUseCaseTests : DescribeSpec({
     }
 
     describe("call") {
-        val input = AccountCreateUseCaseInput("code", "name", 1)
-        val account = Account(code = "code", name = "name", elementType = 1)
+        val input = AccountCreateUseCaseInput("code", "name", 1, user_id = 1)
+        val account = Account(code = "code", name = "name", elementType = 1, user_id = 1)
 
         every { repository.create(account) } returns account.copy(id = 1)
         every { repository.findById(1) } returns account.copy(id = 1)
